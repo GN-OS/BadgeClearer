c4c074c63cad6aac8b5c0ed105858058030c183c
Ge0rges <giokanaan@gmail.com>
2014-01-09 23:53:18 +0200

    added pref bundle with donationa nd twitter link. Fixed filter bug. Added new icons.

--- Makefile
+++ Makefile
@@ -11,3 +11,5 @@ include $(THEOS_MAKE_PATH)/tweak.mk
 after-install::
 	install.exec "killall -9 SpringBoard"
 
+SUBPROJECTS += badgeclearerpreferences
+include $(THEOS_MAKE_PATH)/aggregate.mk
---
+++ badgeclearerpreferences/BadgeClearerPreferences.mm
@@ -0,0 +1,40 @@
+//#import <Preferences/Preferences.h>
+//Tired of having header issues? Just forward-declare stuff!
+@interface PSListController : NSObject { //Good enough
+    id _specifiers;
+}
+
+- (id)loadSpecifiersFromPlistName:(NSString *)name target:(id)target;
+
+@end
+@interface BadgeClearerPreferencesListController: PSListController
+
+@end
+
+@implementation BadgeClearerPreferencesListController
+- (id)specifiers {
+    if(_specifiers == nil) {
+        _specifiers = [[self loadSpecifiersFromPlistName:@"BadgeClearerPreferences" target:self] retain];
+    }
+    return _specifiers;
+}
+
+- (void)donation {
+    
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=LAEUC26VGLX2N"]];
+    
+}
+
+- (void)twitter {
+    
+    if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"twitter://user?screen_name=GN_OS"]]) {
+        
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"twitter://user?screen_name=GN_OS"]];
+    } else {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://twitter.com/GN_OS"]];
+    }
+}
+
+@end
+
+// vim:ft=objc
